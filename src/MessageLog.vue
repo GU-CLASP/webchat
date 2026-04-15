8d093652bca29b5de4942612d19c0e2264d3f9f5
Simon Forsberg <simon.forsberg@gu.se>
2026-04-15 14:07:40 +0200

    Scroll messages to bottom in replay view

--- src/MessageLog.vue
+++ src/MessageLog.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { formatTime } from '@shared/admin';
 import { Message } from '@shared/chat';
+import { nextTick, ref, watch } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   messages: Message[],
 }>()
+const messagesElement = ref<HTMLElement | null>(null);
 
+watch(() => props.messages.length, (newVal, oldVal) => {
+  nextTick(() => {
+    if (!messagesElement.value) {
+      return;
+    }
+    messagesElement.value.scrollTo({
+      top: messagesElement.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  });
+})
 </script>
 <template>
-  <div class="message-feed">
+  <div class="message-feed" ref="messagesElement">
     <article v-for="message in messages" :key="message.id" class="message-card">
       <div class="message-card-head">
         <strong>{{ message.senderName }}</strong>
